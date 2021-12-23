f4716d0dd0b82ba1382fbcce0aacff0126e8054f
Yonatan Bitton <yonatan.bitton@perception-point.io>
2021-12-23 15:23:28 +0200

    updated version to yagmail-0.18.0+pp

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import find_packages
 with open('README.rst') as f:
     LONG_DESCRIPTION = f.read()
 MAJOR_VERSION = '0'
-MINOR_VERSION = '17'
+MINOR_VERSION = '18'
 MICRO_VERSION = '0'
-VERSION = "{}.{}.{}.pp".format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION)
+VERSION = "{}.{}.{}+pp".format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION)
 
 setup(
     name='yagmail',
